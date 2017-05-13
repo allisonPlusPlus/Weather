b29eabfdbdfe5557bbcd8e8a30c359d9c5b78a8f
theallisonbrett <theallisonbrett@gmail.com>
2017-05-13 15:52:16 -0700

    Find users’ longitude and latitude

--- weather.js
+++ weather.js
@@ -3,3 +3,13 @@ $.get("http://ipinfo.io", function (response) {
     $("#address").html(response.city);
     $("#details").html(JSON.stringify(response, null, 4));
 }, "jsonp");
+
+
+if ("geolocation" in navigator){ //check geolocation available
+    //try to get user current location using getCurrentPosition() method
+    navigator.geolocation.getCurrentPosition(function(position){
+            console.log("Found your location \nLat : "+position.coords.latitude+" \nLang :"+ position.coords.longitude);
+        });
+}else{
+    console.log("Browser doesn't support geolocation!");
+}
