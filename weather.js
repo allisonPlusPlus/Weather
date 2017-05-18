c3904d0e72fd66d2f1f4054249a15e84200147f6
theallisonbrett <theallisonbrett@gmail.com>
2017-05-18 15:49:34 -0700

    Define two new variables long and lat

--- weather.js
+++ weather.js
@@ -5,10 +5,11 @@ $.get("https://ipinfo.io", function (response) {
 }, "jsonp");
 
 function getWeather() {
-
 if ("geolocation" in navigator){ //check geolocation available
     //try to get user current location using getCurrentPosition() method
     navigator.geolocation.getCurrentPosition(function(position){
+      var lat = position.coords.latitude;
+      var long = position.coords.longitude;
             console.log("Found your location \nLat : "+position.coords.latitude+" \nLang :"+ position.coords.longitude);
              $.get("https://api.darksky.net/forecast/9374c70872b17665a8bb166f1d503135/37.8267,-122.4233", function (response) {
                $("#degrees").html(response.currently.temperature);
