a49c42198a6659d880aa57a8f058244c30d81333
theallisonbrett <theallisonbrett@gmail.com>
2017-05-18 15:46:59 -0700

    Move darksky API inside IF statement of getWeather function

--- weather.js
+++ weather.js
@@ -10,15 +10,14 @@ if ("geolocation" in navigator){ //check geolocation available
     //try to get user current location using getCurrentPosition() method
     navigator.geolocation.getCurrentPosition(function(position){
             console.log("Found your location \nLat : "+position.coords.latitude+" \nLang :"+ position.coords.longitude);
+             $.get("https://api.darksky.net/forecast/9374c70872b17665a8bb166f1d503135/37.8267,-122.4233", function (response) {
+               $("#degrees").html(response.currently.temperature);
+               $("#weathersummary").html(response.currently.summary);
+             }, "jsonp");
         });
 }else{
     console.log("Browser doesn't support geolocation!");
 }
-
- $.get("https://api.darksky.net/forecast/9374c70872b17665a8bb166f1d503135/37.8267,-122.4233", function (response) {
-   $("#degrees").html(response.currently.temperature);
-   $("#weathersummary").html(response.currently.summary);
- }, "jsonp");
 }
 
 getWeather();
